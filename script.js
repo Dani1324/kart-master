e59f5e1758e3f7bd38d20fed3bde99800fb0e997
Dani1324 <danieleronchini2@gmail.com>
2022-07-17 15:56:07 +0200

    1.03

--- script.js
+++ script.js
@@ -16,32 +16,3 @@ $("a#ciao").on('click', function(event) {
     });
   }
 });
-
-// Header resize
-let header = document.getElementsByClassName('.header');
-const width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-var iOS = !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent);
-
-window.addEventListener('scroll', function(){
-
-  if (width > 1025) {
-    if(document.documentElement.scrollTop > 50){
-      $(".header").css("background-color", "#292829");
-      $(".header").css("padding", "1.5vw 0vw");
-    }
-    else {
-      $(".header").css("background-color", "rgba(255, 255, 255, 0)");
-      $(".header").css("padding", "2.5vw 0vw");
-    }
-  }
-  else {
-    if(document.documentElement.scrollTop > 20){
-      $(".header").css("background-color", "rgba(255, 255, 255, 1)");
-      $(".header").css("padding", "1.5vh 10vw 1.5vh 10vw");
-    }
-    else {
-      $(".header").css("background-color", "rgba(255, 255, 255, 0)");
-      $(".header").css("padding", "2.5vh 10vw 2.5vh 10vw");
-    }
-  }
-})
